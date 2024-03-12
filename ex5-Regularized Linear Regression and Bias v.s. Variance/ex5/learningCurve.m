da2b478227190cebae46a8cfca5bcbf9ee2aae55
LinMiao <2418513325@qq.com>
2024-03-12 14:19:44 +0800

    Add ex5-Regularized Linear Regression and Bias v.s. Variance

---
+++ ex5-Regularized Linear Regression and Bias v.s. Variance/ex5/learningCurve.m
@@ -0,0 +1,84 @@
+function [error_train, error_val] = ...
+    learningCurve(X, y, Xval, yval, lambda)
+%LEARNINGCURVE Generates the train and cross validation set errors needed 
+%to plot a learning curve
+%   [error_train, error_val] = ...
+%       LEARNINGCURVE(X, y, Xval, yval, lambda) returns the train and
+%       cross validation set errors for a learning curve. In particular, 
+%       it returns two vectors of the same length - error_train and 
+%       error_val. Then, error_train(i) contains the training error for
+%       i examples (and similarly for error_val(i)).
+%
+%   In this function, you will compute the train and test errors for
+%   dataset sizes from 1 up to m. In practice, when working with larger
+%   datasets, you might want to do this in larger intervals.
+%
+
+% Number of training examples
+m = size(X, 1);
+
+% You need to return these values correctly
+error_train = zeros(m, 1);
+error_val   = zeros(m, 1);
+
+% ============================= YOUR CODE HERE ============================
+% Instructions: Fill in this function to return training errors in 
+%               error_train and the cross validation errors in error_val. 
+%               i.e., error_train(i) and 
+%               error_val(i) should give you the errors
+%               obtained after training on i examples.
+%
+% Note: You should evaluate the training error on the first i training
+%       examples (i.e., X(1:i, :) and y(1:i)).
+%
+%       For the cross-validation error, you should instead evaluate on
+%       the _entire_ cross validation set (Xval and yval).
+%
+% Note: If you are using your cost function (linearRegCostFunction)
+%       to compute the training and cross validation error, you should 
+%       call the function with the lambda argument set to 0. 
+%       Do note that you will still need to use lambda when running
+%       the training to obtain the theta parameters.
+%
+% Hint: You can loop over the examples with the following:
+%
+%       for i = 1:m
+%           % Compute train/cross validation errors using training examples 
+%           % X(1:i, :) and y(1:i), storing the result in 
+%           % error_train(i) and error_val(i)
+%           ....
+%           
+%       end
+%
+
+% ---------------------------- Sample Solution ----------------------------
+
+for i = 1:m
+    theta = trainLinearReg(X(1:i,:), y(1:i), lambda);
+    error_train(i) = 1/2/i*sum((X(1:i,:)*theta-y(1:i)).^2);
+    error_val(i) = 1/2/size(Xval,1)*sum((Xval*theta-yval).^2);
+end
+
+% ---------------------------- Option Exercise ----------------------------
+% learning curves for Polynomial Regression with randomly selected examples 
+% ------------------------ Run Section Part 1、6、7 ------------------------
+
+% for i = 1:m
+%     rand_error_train = zeros(1,50);
+%     rand_error_val = zeros(1,50);
+%     for j = 1:50
+%        rand_dataset = datasample([X y],i);
+%        rand_valdataset = datasample([Xval yval],i);
+%        rand_theta = trainLinearReg(rand_dataset(1:i,1:end-1),rand_dataset(1:i,end),lambda);
+%        rand_error_train(j) = 1/2/i*sum((rand_dataset(1:i,1:end-1)*rand_theta-rand_dataset(1:i,end)).^2);
+%        rand_error_val(j) = 1/2/size(rand_valdataset,1)*sum((rand_valdataset(:,1:end-1)*rand_theta-rand_valdataset(:,end)).^2);
+%     end
+%     error_train(i) = mean(rand_error_train);
+%     error_val(i) = mean(rand_error_val);
+% end
+
+% -------------------------------------------------------------------------
+
+% =========================================================================
+
+end
